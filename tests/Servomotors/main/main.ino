e2c5c9b1432cbf2b40c745fdba5227e242af3436
Ignazio Neto Dell'Acqua <ignaiio123@gmail.com>
2024-04-18 21:10:43 +0200

    Servo update

--- tests/Servomotors/main/main.ino
+++ tests/Servomotors/main/main.ino
@@ -9,15 +9,14 @@
   // addMovement requires the pin of the interested servo, the number of movement we want to add , followed by the path 
   // Every time we add a movement with addMovement we overwrite the previous path added 
 
-
 //SERVO
-const int SERVOS = 2;
-const int INITIAL_PIN_SERVOS = 6;
-const int mouthPin = 6;
-const int armPin1 = 7;
-const int armPin2 = 8;
-const int armPin3 = 9;
-const int typeServo = 0; //0 for g90 , 1 for others
+const int SERVOS = 4;
+const int INITIAL_PIN_SERVOS = 2;
+const int mouthPin = 2 - INITIAL_PIN_SERVOS;
+const int armPin1 = 3 - INITIAL_PIN_SERVOS;
+const int armPin2 = 4 - INITIAL_PIN_SERVOS;
+const int armPin3 = 5 - INITIAL_PIN_SERVOS;
+const int typeServo = 1; //0 for g90 , 1 for others
 
 //IR
 const int irPin = 8;
@@ -30,37 +29,41 @@ static unsigned long previousMillisPrint = 0;
 
 unsigned long currentMillis;
 
+Movement loopPath[3][6] = {
+  { {1,0,90} , {50,5,90} , {-90,4,90} , {10,2,90}, {-30,6,90} , {1,10,90} },
+  { {1,0,90} , {-40,5,90} , {90,4,60} , {10,2,80}, {-60,6,30} , {1,10,90} },
+  { {1,0,90} , {20,5,75} , {-40,4,90} , {-60,2,80}, {30,6,45} , {1,10,90} }
+};
+
 void setup() {
 
   Serial.begin(9600);
-  initializeServos(SERVOS,INITIAL_PIN_SERVOS);
+  initializeServos(SERVOS,INITIAL_PIN_SERVOS,typeServo);
   pinMode(irPin, INPUT);
-  
-  Movement path_1[6] = { {1,0,90} , {50,25,90} , {-90,10,90} , {10,15,90}, {-30,40,90} , {1,25,90} }; 
-  addMovement(mouthPin, 6, path_1, typeServo); 
-  
-  Movement path_2[6] = { {1,0,90} , {-40,15,90} , {90,10,60} , {10,5,80}, {-60,20,30} , {1,25,90} }; 
-  addMovement(armPin1, 6, path_2, typeServo);
-  
-  Movement path_3[6] = { {1,0,90} , {-20,15,60} , {40,10,90} , {60,5,80}, {-30,20,45} , {1,25,90} }; 
-  addMovement(armPin2, 6, path_3, typeServo);
-  
-  Movement path_4[6] = { {1,0,90} , {20,15,75} , {-40,10,90} , {-60,5,80}, {30,20,45} , {1,25,90} }; 
-  addMovement(armPin3, 6, path_4, typeServo);
 
+  addMovement(mouthPin, 6, loopPath[0] , typeServo); 
+  addMovement(armPin1, 6, loopPath[1], typeServo);
+  addMovement(armPin2, 6, loopPath[2], typeServo);
 }
 
 void loop() {
 
+  if(isComplete(mouthPin)) addMovement(mouthPin, 6, loopPath[0], typeServo);
+  if(isComplete(armPin1)) addMovement(armPin1, 6, loopPath[1], typeServo);
+  if(isComplete(armPin2)) addMovement(armPin2, 6, loopPath[2], typeServo);
+ 
   currentMillis = millis();
 
   if (currentMillis - previousMillisIR >= irInterval) {
     previousMillisIR = currentMillis;
     irValue = digitalRead(irPin);
     
-    if (!irValue && irOldValue) {
-      //setDelayedPosition(0, 45, 5000);
-      //setDelayedPosition(1, 90, 5000);
+    if (!irValue && irOldValue) {    
+      Movement path_3[6] = { {1,0,90} , {-20,5,60} , {40,4,90} , {60,2,80}, {-30,6,45} , {1,10,90} }; 
+      addMovement(armPin2, 6, path_3, typeServo);
+      
+      Movement path[4] = { {1,0,90} , {40,7.5,60} , {-70,10,90} , {1,5,90} }; 
+      addMovement(armPin3, 4, path, typeServo);
     }
 
     irOldValue = irValue;
@@ -78,5 +81,3 @@ void loop() {
   delay(10);
 
 }
-
-
--- tests/Servomotors/main/servo_controller.h
+++ tests/Servomotors/main/servo_controller.h
@@ -10,112 +10,119 @@ int NUM_SERVOS;
 int INITIAL_PIN;
 
 struct ServoData {
-  Servo servo;
-  int previousAngle;
-  int angle; //must be integer
-  unsigned long previousMillis;
-  unsigned long previousDelayMillis;
-  int numMovements;
-  int movementIndex;
-  int angles[MAX_MOVEMENTS]; //degree
-  int delays[MAX_MOVEMENTS]; //sec
-  int speeds[MAX_MOVEMENTS]; //degree/sec
+    Servo servo;
+    int previousAngle;
+    int angle; //must be integer
+    unsigned long previousMillis;
+    unsigned long previousDelayMillis;
+    int numMovements;
+    int movementIndex;
+    int angles[MAX_MOVEMENTS]; //degree
+    int delays[MAX_MOVEMENTS]; //sec
+    int speeds[MAX_MOVEMENTS]; //degree/sec
 };
 
-struct Movement{
-  int ang;
-  int delay;
-  int speed;
+struct Movement {
+    int ang;
+    int delay;
+    int speed;
 };
 
 ServoData servos[MAX_SERVO];
 
-void initializeServos(int num_servos, int initialPin) {
-  NUM_SERVOS = num_servos;
-  INITIAL_PIN = initialPin;
-  for (int i = 0; i < NUM_SERVOS; i++) {
-    servos[i].servo.attach(INITIAL_PIN + i); //servos should be one after the other
-    servos[i].previousAngle = 0;
-    servos[i].angle = 0;
-    servos[i].previousMillis = 0;
-    servos[i].previousDelayMillis = 0;
-    servos[i].numMovements = 0;   
-    servos[i].movementIndex = 0;
-    memset(servos[i].angles, 0, sizeof(servos[i].angles));
-    memset(servos[i].delays, 0, sizeof(servos[i].delays));
-  }
+void addMovement(int servoIndex, int num_movement, Movement path[], int type) {
+    servos[servoIndex].movementIndex = 0;
+    servos[servoIndex].numMovements = num_movement;
+    for (int i = 0; i < num_movement; i++) {
+        if (type == 1) {
+            servos[servoIndex].angles[i] = path[i].ang + 90;
+            if (servos[servoIndex].angles[i] == 0) {
+                servos[servoIndex].angles[i] = 1;
+            }
+        }
+        else {
+            servos[servoIndex].angles[i] = path[i].ang;
+        }
+        servos[servoIndex].delays[i] = path[i].delay;
+        if (path[i].speed < MIN_SPEED) {
+            servos[servoIndex].speeds[i] = MIN_SPEED;
+        }
+        else {
+            servos[servoIndex].speeds[i] = path[i].speed;
+        }
+    }
+    servos[servoIndex].angle = servos[servoIndex].angles[0];
+}
+
+void initializeServos(int num_servos, int initialPin, int type) {
+    NUM_SERVOS = num_servos;
+    INITIAL_PIN = initialPin;
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        servos[i].servo.attach(INITIAL_PIN + i); //servos should be one after the other
+        servos[i].previousMillis = 0;
+        servos[i].previousDelayMillis = 0;
+        servos[i].numMovements = 0;
+        servos[i].movementIndex = 0;
+        memset(servos[i].angles, 0, sizeof(servos[i].angles));
+        memset(servos[i].delays, 0, sizeof(servos[i].delays));
+        if (type == 1) {
+            servos[i].servo.write(91);
+            servos[i].previousAngle = 91;
+            servos[i].angle = 91;
+        }
+        else {
+            servos[i].servo.write(1);
+            servos[i].previousAngle = 1;
+            servos[i].angle = 1;
+        }
+    }
 }
 
 void updateServos(unsigned long currentMillis) {
-  //Move servos
-  for (int i = 0; i < NUM_SERVOS; i++) {
-    if (servos[i].previousAngle != servos[i].angle) {
-      if(servos[i].speeds[servos[i].movementIndex] >= MAX_SPEED){
-        servos[i].servo.write(servos[i].angle);
-        servos[i].previousAngle = servos[i].angle;
-      }
-      else{
-        int direction = (servos[i].angle > servos[i].previousAngle) ? 1 : -1 ;
-        if (currentMillis - servos[i].previousMillis >= (1000/servos[i].speeds[servos[i].movementIndex])) {
-          servos[i].previousMillis = currentMillis; 
-          servos[i].servo.write(servos[i].previousAngle + direction);
-          servos[i].previousAngle += direction;
+    //Move servos
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        if (servos[i].previousAngle != servos[i].angle) {
+            if (servos[i].speeds[servos[i].movementIndex] >= MAX_SPEED) {
+                servos[i].servo.write(servos[i].angle);
+                servos[i].previousAngle = servos[i].angle;
+            }
+            else {
+                int direction = (servos[i].angle > servos[i].previousAngle) ? 1 : -1;
+                if (currentMillis - servos[i].previousMillis >= (1000 / servos[i].speeds[servos[i].movementIndex])) {
+                    servos[i].previousMillis = currentMillis;
+                    servos[i].servo.write(servos[i].previousAngle + direction);
+                    servos[i].previousAngle += direction;
+                }
+            }
         }
-      }
     }
-  }
-  //Update Angles
-  for (int i = 0; i < NUM_SERVOS; i++) {  
-    if (servos[i].movementIndex < servos[i].numMovements - 1) {
-      if ((currentMillis - servos[i].previousDelayMillis)/1000 >= servos[i].delays[servos[i].movementIndex+1]) {
-          servos[i].movementIndex++;
-          servos[i].previousDelayMillis = currentMillis;
-          servos[i].angle = servos[i].angles[servos[i].movementIndex];
-      }
+    //Update Angles
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        if (servos[i].movementIndex < servos[i].numMovements - 1) {
+            if ((currentMillis - servos[i].previousDelayMillis) / 1000 >= servos[i].delays[servos[i].movementIndex + 1]) {
+                servos[i].movementIndex++;
+                servos[i].previousDelayMillis = currentMillis;
+                servos[i].angle = servos[i].angles[servos[i].movementIndex];
+            }
+        }
     }
-  }
 }
 
-void addMovement(int pin, int num_movement, Movement path[], int type) {
-  int servoIndex = INITIAL_PIN - pin;
-  servos[servoIndex].movementIndex = 0;
-  servos[servoIndex].numMovements = num_movement;
-  for (int i = 0; i < num_movement; i++) {
-    if (type == 1){      
-      servos[servoIndex].angles[i] = path[i].ang + 90;
-      if(servos[servoIndex].angles[i] == 0){
-        servos[servoIndex].angles[i] = 1;
-      }
-    }
-    else{    
-      servos[servoIndex].angles[i] = path[i].ang;
-    }
-    servos[servoIndex].delays[i] = path[i].delay;
-    if(path[i].speed < MIN_SPEED){
-      servos[servoIndex].speeds[i] = MIN_SPEED;
-    }
-    else{
-      servos[servoIndex].speeds[i] = path[i].speed;
-    }
-  }    
-  servos[servoIndex].angle = servos[servoIndex].angles[0];
+bool isComplete(int servoIndex) {
+    return servos[servoIndex].movementIndex == (servos[servoIndex].numMovements - 1) && servos[servoIndex].previousAngle == servos[servoIndex].angle;
 }
 
 void printServoData() {
-  for (int i = 0; i < NUM_SERVOS; i++) {
-      Serial.print("Servo ");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.print("Previous Angle: ");
-      Serial.print(servos[i].previousAngle);
-      Serial.print(", Current Angle: ");
-      Serial.print(servos[i].angle);
-      Serial.print(", Movement Index: ");
-      Serial.println(servos[i].movementIndex);
-      Serial.print("\n");
-  }
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        Serial.print("Servo ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.print(", Current Angle: ");
+        Serial.print(servos[i].angle);
+        Serial.print(", Is Complete: ");
+        Serial.println(isComplete(i));
+        Serial.print("\n");
+    }
 }
 
 #endif
-
-
