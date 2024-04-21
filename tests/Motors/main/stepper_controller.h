78b1b3e8891f48f06d36827a2ac84f59b29b45c8
Ignazio Neto Dell'Acqua <ignaiio123@gmail.com>
2024-04-21 14:45:55 +0200

    Added stepper controller

---
+++ tests/Motors/main/stepper_controller.h
@@ -0,0 +1,90 @@
+#ifndef STEPPER_CONTROLLER_H
+#define STEPPER_CONTROLLER_H
+
+//Hyperparameters
+#define NUM_STEPPER 2
+#define INITIAL_PIN 2
+#define STEPS_PER_REVOLUTION 100
+#define LENGTH 40 //centimeters
+#define MAX_DEBOUNCING 3 //centimeters
+#define RADIOUS_GEAR 0.01 //meters
+#define FLOATING_TIME 1000 //milliseconds
+//
+
+#include <Stepper.h>
+
+struct StepperData {
+  Stepper stepper;
+  int percentage; //integer
+  int bounceStep; //in meters
+  int timeToFloat;
+  int bounceVelocity; //rpm
+  bool goUp;
+  unsigned long previousMillis;
+
+  StepperData() : stepper(STEPS_PER_REVOLUTION, 0, 0, 0, 0) {}
+
+  void setPins(int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4) {
+    stepper = Stepper(STEPS_PER_REVOLUTION, motor_pin_1, motor_pin_2, motor_pin_3, motor_pin_4);
+  }
+
+  void setSpeed(int rpm) {
+    stepper.setSpeed(rpm);
+  }
+
+  void step(int steps) {
+    stepper.step(steps);
+  }
+};
+
+StepperData steppers[NUM_STEPPER];
+
+void initializeSteppers() {
+  for (int i = 0; i < NUM_STEPPER; i++) {
+    steppers[i].setPins(INITIAL_PIN + 4*i, INITIAL_PIN + 1 + 4*i, INITIAL_PIN + 2 + 4*i, INITIAL_PIN + 3 + 4*i);
+    steppers[i].percentage = 0;
+    steppers[i].bounceStep = 0;
+    steppers[i].timeToFloat = 0;
+    steppers[i].bounceVelocity = 0;
+    steppers[i].previousMillis = 0;
+    steppers[i].goUp = true;
+  }
+}
+
+void addMovementStepper(int stepperIndex, int percentage, int velocity, int bounceStep, int bounceVelocity, unsigned long currentMillis) {
+  double step = 0.01 * ((LENGTH - MAX_DEBOUNCING) / (6.28 * RADIOUS_GEAR)) * STEPS_PER_REVOLUTION * 0.01 * (percentage - steppers[stepperIndex].percentage);
+  if (!steppers[stepperIndex].goUp) {
+    step -= steppers[stepperIndex].bounceStep;
+  }
+
+  steppers[stepperIndex].setSpeed(velocity);
+  steppers[stepperIndex].step(step);
+
+  steppers[stepperIndex].percentage = percentage;
+  steppers[stepperIndex].previousMillis = currentMillis;
+  steppers[stepperIndex].bounceStep = bounceStep > MAX_DEBOUNCING ? (0.01 * (MAX_DEBOUNCING / (6.28 * RADIOUS_GEAR)) * STEPS_PER_REVOLUTION) : (0.01 * (bounceStep / (6.28 * RADIOUS_GEAR)) * STEPS_PER_REVOLUTION);
+  steppers[stepperIndex].timeToFloat = fmod(1000 * (60.0 / velocity) * (step / STEPS_PER_REVOLUTION), 1.0);
+  steppers[stepperIndex].bounceVelocity = bounceVelocity;
+  steppers[stepperIndex].goUp = true;
+}
+
+void updateSteppers(unsigned long currentMillis) {
+  for (int i = 0; i < NUM_STEPPER; i++) {
+    if (steppers[i].bounceStep > 0 && (currentMillis - steppers[i].previousMillis) > steppers[i].timeToFloat) {
+
+      steppers[i].setSpeed(steppers[i].bounceVelocity);
+
+      if (steppers[i].goUp) {
+        steppers[i].step(steppers[i].bounceStep);
+        steppers[i].goUp = false;
+      } else {
+        steppers[i].step(-steppers[i].bounceStep);
+        steppers[i].goUp = true;
+      }
+      steppers[i].previousMillis = currentMillis;
+      steppers[i].timeToFloat = FLOATING_TIME;
+    }
+  }
+}
+
+#endif
--- tests/Motors/onlyServo/onlyServo.ino
+++ tests/Motors/onlyServo/onlyServo.ino
@@ -1,10 +1,16 @@
+#include <Arduino.h>
+#include "servo_controller.h"
+#define MAX_COMMAND_LENGTH 140
+#define MAX_SERVO 11
+#define INITIAL_PIN_SERVOS 2
+
 /*
  This function serves as a serial command handler. 
  Serial command arguments **must** be separated by a **space** in the following order:
 
- * 1. Command number: 0 , 1 or 2.
+ * 1. Command number: 0 or 1.
  *
- *    - 0: Add new movements to a servo motor. If this is the command chosed, the following should be:  
+ *    - 0: Add new movements to a servo. If this is the command chosed, the following should be:  
  *            
  *          2. Servo number: From 0 to the total number of servos minus 1. (Servos are meant to be connected in consecutive pins)
  * 
@@ -18,40 +24,23 @@
  *    
  *    Example of input: 0 0 2 90 0 90 0 5 100 -> This command cause the Servo no 0 to move to 90 degrees immediatelly (since 0 delay) at 90 deg/sec velocity and then after 5 seconds from the prevoius movement move to 0 degrees instantly (since 100 velocity), then finish
  *    
- *    - 1: Inquire whether the servo motor has finished its execution (return 1 if the movements are completed, 0 if not). If this is the command chosed the following should be: 
+ *    - 1: Inquire whether the servo has finished its execution (return 1 if the movements are completed, 0 if not). If this is the command chosed the following should be: 
  *
  *          2. Servo number: From 0 to the total number of servos minus 1. (Servos are meant to be connected in consecutive pins)
  *
  *    Example of input: 1 2 -> Inquire if the Servo no 2 has finished or not
-        
- *    - 2: Add new movements to a steeper motor controlling the displacement wrt the origin of an axis by a gear . If this is the command chosed, the following should be:  
- *            
- *          2. Stepper number: From 0 to the total number of stepper minus 1. (Stepper are meant to be connected in consecutive pins)
- * 
- *          3. Percentage (integer value) of displacent of the axis (0 if is at the origin, 100 is it is at maximum displacement wrt the origin).
- * 
- *          4. Velocity of the gear (rpm)
- * 
- *          5. Lenght of the bouncing that the axis start to do after reaching the wanted displacement in centimeters (0 for no bouncing) (if this value is greather then MAX_BOUNCING is saturate at this value)
- * 
- *          6. Velocity of the gear for the bouncing (rpm)
- *    
- *    Example of input: 2 1 90 100 2 200 -> This command cause the Stepper no 1 to move the axis to 90% of maximum displacement moving the gear at 100 rpm , then after reaching the displacemnt it start bouncing up and down of 2 centimeters with gear speed of 200 rpm
- *    
+ *
  */
 
-#include <Arduino.h>
-#include "servo_controller.h"
-#include "stepper_controller.h"
-#define MAX_COMMAND_LENGTH 140
-
 unsigned long currentMillis;
+int servoIndex;
+int totMovement;
+int i,j;
 int command[MAX_COMMAND_LENGTH];
 
 void setup() {
   Serial.begin(9600);
-  initializeServos();
-  initializeSteppers();
+  initializeServos(MAX_SERVO, INITIAL_PIN_SERVOS);
 }
 
 void loop() {
@@ -66,28 +55,25 @@ void loop() {
     }
 
     if (command[0] == 0) {
-      int totMovement = command[2];
+      servoIndex = command[1];
+      totMovement = command[2];
       Movement path[totMovement];
-      int i = 3;
-      int j = 0;
+      i = 3;
+      j = 0;
       while (i < 3*(totMovement+1)) {
         path[j].ang = command[i++];
         path[j].del = command[i++];
         path[j++].speed = command[i++];
       }
-      addMovementServo(command[1], path, totMovement, currentMillis);
+      addMovement(servoIndex, path, totMovement, currentMillis);
     }
 
     if (command[0] == 1) {
-      Serial.println(isCompleteServo(command[1]));
-    }
-
-    if (command[0] == 2) {
-      addMovementStepper(command[1], command[2] , command[3], command[4] , command[5] ,currentMillis);
+      servoIndex = command[1];
+      Serial.println(isComplete(servoIndex));
     }
   }
 
-  updateSteppers(currentMillis);
   updateServos(currentMillis);
   delay(10);
 }
