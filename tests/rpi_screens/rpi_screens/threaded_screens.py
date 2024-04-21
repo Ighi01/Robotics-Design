3a29ad0b5ede6d20d6b1768184a095b91734a4f8
kiinami <kinami@posteo.net>
2024-04-21 09:32:00 +0000

    screens that work

---
+++ tests/rpi_screens/rpi_screens/threaded_screens.py
@@ -0,0 +1,88 @@
+from time import sleep
+from adafruit_rgb_display.st7735 import ST7735R
+from threading import Thread
+from digitalio import DigitalInOut
+import board
+from PIL import Image, ImageDraw, ImageSequence
+
+spi = board.SPI()
+
+class Screen:
+    device: ST7735R
+    current_thread: Thread
+    stop: bool = False
+    
+    def __init__(cls, cs: DigitalInOut, dc: DigitalInOut, rst: DigitalInOut):
+        cls.device = ST7735R(
+            spi, 
+            rotation=0,
+            cs=cs,
+            dc=dc,
+            rst=rst,
+            baudrate=24000000,
+            bgr=True
+        )
+        cls.current_thread = None
+        
+    def _stop(self):
+        self.stop = True
+        if self.current_thread:
+            self.current_thread.join()
+        self.stop = False
+        
+    def _blank(self):
+        if self.device.rotation % 180 == 90:
+            height = self.device.width  # we swap height/width to rotate it to landscape!
+            width = self.device.height
+        else:
+            width = self.device.width  # we swap height/width to rotate it to landscape!
+            height = self.device.height
+        image = Image.new("RGB", (width, height))
+
+        # Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
+
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+        self.device.image(image)
+        
+    def blank(self):
+        self._stop()
+        self.current_thread = Thread(target=self._blank)
+        self.current_thread.start()
+        
+    def _sad(self):
+        images = []
+        with Image.open('sad.gif') as im:
+            for frame in ImageSequence.Iterator(im):
+                frame = frame.convert('RGB')
+                images.append(frame)
+        while not self.stop:
+            for frame in images:
+                self.device.image(frame)
+                
+    def sad(self):
+        self._stop()
+        self.current_thread = Thread(target=self._sad)
+        self.current_thread.start()
+        
+
+if __name__ == '__main__':
+    left_screen = Screen(DigitalInOut(board.D17), DigitalInOut(board.D25), DigitalInOut(board.D24))
+    right_screen = Screen(DigitalInOut(board.D27), DigitalInOut(board.D6), DigitalInOut(board.D5))
+    
+    print('Blanking screens')
+    left_screen.blank()
+    right_screen.blank()
+    
+    sleep(2)
+    
+    print('Showing sad face')
+    left_screen.sad()
+    right_screen.sad()
+    
+    sleep(5)
+    
+    print('Blanking screens')
+    left_screen.blank()
+    right_screen.blank()
