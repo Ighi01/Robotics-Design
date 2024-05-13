a3a1d7dd76bb644bc9b5d089a6a33c1dea2a2bd8
kiinami <kinami@posteo.net>
2024-05-13 19:21:59 +0200

    better sound

--- tests/sound/sound.py
+++ tests/sound/sound.py
@@ -1,6 +1,15 @@
-import simpleaudio as sa
+import pygame
+from time import sleep
 
-filename = 'a.wav'
-wave_obj = sa.WaveObject.from_wave_file(filename)
-play_obj = wave_obj.play()
-play_obj.wait_done()
+pygame.mixer.init(channels=2)
+right_channel = pygame.mixer.Channel(0)
+right_channel.set_volume(1.0, 0.0)
+left_channel = pygame.mixer.Channel(1)
+left_channel.set_volume(0.0, 1.0)
+
+sound = pygame.mixer.Sound("sample-6s.wav")
+
+right_channel.play(sound)
+sleep(1)
+left_channel.play(sound)
+sleep(8)
