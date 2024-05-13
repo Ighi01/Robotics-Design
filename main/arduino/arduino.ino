2cf27619ca0820947e8f6c1a74f1a96552740261
Ignazio Neto Dell'Acqua <ignaiio123@gmail.com>
2024-05-13 22:41:31 +0200

    merge

---
+++ main/arduino/arduino.ino
@@ -0,0 +1,121 @@
+/*
+  This function serves as a serial command handler for controlling multiple servo motors simultaneously
+  Serial command arguments **must** be separated by a **space** in the following order:
+
+ * 1. Command number: 0 or 1.
+ *
+ *    - 0: Add new movements to the servo motors. If this is the command chosed, the following should be:  
+ *           
+ *          2. Number of Servo: From 1 to the total number of servos. For each servos that you want to move you must specify those commands:
+ *  
+ *               3. Servo index: From 0 to the total number of servos minus 1. (Servos are meant to be connected in consecutive pins)
+ * 
+ *               4. Number of movements to assign to the servo (0 to 20).
+ * 
+ *               5. For each movement assigned to the servo:
+ *                   - Angle to assign to the servo (in degrees, must be integer).
+ *                   - Delay after the previous movement to start the movement (in seconds). Use 0 for immediate movement.
+ *                   - Movement speed in degrees per second . Use 100 for instant transition.
+ *               NOTE: The number of movements specified must match the number of triplets provided. All those arguments must be separated by space ,too
+ * 
+ *          NOTE: The number of servo specified must match the number of servos then provided. All those arguments must be separated by space ,too
+ *
+ *      Example of input: 0 2 0 2 90 0 90 0 5 100 3 1 60 0 30 -> This command cause 2 servo to move simultaneously: the Servo no 0 to move to 90 degrees immediatelly (since 0 delay) at 90 deg/sec velocity and then after 5 seconds 
+ *                                                           from the prevoius movement move to 0 degrees instantly (since 100 velocity), then finish; 
+ *                                                           in parallel it also cause the Servo no3 to move of 60 degrees immediatelly at 30 deg/sec velocity, then finish
+ *    0 4 0 4 90 0 75 0 2 100 90 2 75 0 2 100 1 4 90 0 75 0 2 100 90 2 75 0 2 100 3 3 180 0 100 0 4 75 90 3 50 2 2 180 0 90 0 5 50
+ *    - 1: Inquire whether the given servo motors are all finished its execution (return 1 if all the movements of all the given servos are completed, 0 if not). If this is the command chosed the following should be: 
+ *
+ *          2. Number of Servo: From 1 to the total number of servos. For each servos that you want to move you must specify those commands:
+ *               
+ *                3. Servo index: From 0 to the total number of servos minus 1. (Servos are meant to be connected in consecutive pins)
+ *
+ *      Example of input: 1 3 2 3 5 -> This command inquire if 3 have finished their movements: Servo no 2 , Servo no 3 and Servo no 5
+ * DA MODIFICARE 
+ * 1. Number of Stepper: From 1 to the total number of stepper. For each stepper that you want to move you must specify those commands:
+ * 
+ *       2. Stepper number: From 0 to the total number of stepper minus 1. (Stepper are meant to be connected in consecutive pins)
+ * 
+ *       3. Percentage (integer value) of displacent of the axis (0 if is at the origin, 100 is it is at maximum displacement wrt the origin)
+ * 
+ *       4. Velocity of the gear (MIN SPEED 15, MAX SPEED 30)
+ * 
+ *       5. Lenght of the bouncing that the axis start to do after reaching the wanted displacement in millimeters (0 for no bouncing) (if this value is greather then MAX_BOUNCING = 0.5 centimeter is saturate at this value)
+ * 
+ *       6. Velocity of the gear for the bouncing (MIN SPEED 15, MAX SPEED 30)
+ * 
+ * NOTE: The number of stepper specified must match the number of stepper then provided. All those arguments must be separated by space ,too
+ *
+ * Example of input: 2 0 90 20 5 30 1 10 30 3 20 -> This command cause 2 stepper to move simultaneously: the Stepper no 0 to move the axis to 90% of maximum displacement moving the gear at 20 speed , then after reaching the displacemnt it start bouncing up and down of 5 millimiterss with gear speed of 30;
+ *                                                                                                            in parallel it also cause the Stepper no 1 to move the axis to 10% of maximum displacement moving the gear at 30 speed , then after reaching the displacemnt it start bouncing up and down of 3 millimiterss with gear speed of 20
+ */
+
+#include <Arduino.h>
+#include "servo_controller.h"
+#include "stepper_controller.h"
+#define MAX_COMMAND_LENGTH 500
+
+unsigned long currentMillis;
+int command[MAX_COMMAND_LENGTH];
+
+void setup() {
+  Serial.begin(9600);
+  initializeServos();
+  //initializeStepper();
+  while (Serial.available() <= 0) {
+    Serial.println("ack");
+    delay(100);
+  }
+}
+
+void loop() {
+  currentMillis = millis();
+
+  if (Serial.available() > 0) {
+    int commandIndex = 0;
+
+    while (Serial.available() > 0) {
+      command[commandIndex] = Serial.parseInt();
+      commandIndex++;
+    }
+
+    if (command[0] == 0) {
+      int i = 1;
+      int totServo = command[i++];
+
+      for(int servo = 0; servo < totServo; servo ++){
+        int servoIndex = command[i++];
+        int totMovement = command[i++];
+        Movement path[totMovement];
+        int h = i;
+        int j = 0;
+        while (i < (3*totMovement + h)) {
+          path[j].ang = command[i++];
+          path[j].del = command[i++];
+          path[j++].speed = command[i++];
+        }
+        addMovementServo(servoIndex, path, totMovement);
+      }
+    }
+
+    if (command[0] == 1) {
+      int i = 1;
+      int totServo = command[i++];
+      int result = true;
+      while(i< (totServo + 1)){
+        if(!isCompleteServo(command[i++])){
+          result = false;
+          break;
+        }
+      }
+      Serial.println(result);
+    }
+
+    if(command[0] == 2){
+      addMovementStepper(command[1], command[2] , command[3], command[4] ,currentMillis);
+    }
+  }
+
+  updateServos(currentMillis);
+  updateStepper(currentMillis);
+}
---
+++ main/arduino/servo_controller.h
@@ -0,0 +1,135 @@
+#ifndef SERVO_CONTROLLER_H
+#define SERVO_CONTROLLER_H
+
+//Hyperparameters
+#define NUM_SERVOS 5
+#define MAX_MOVEMENTS 10
+
+#define PROVIDE_ONLY_LINEAR_MOVEMENT 
+#include <ServoEasing.hpp> 
+
+struct ServoData {
+  ServoEasing servo;
+  int previousAngle;
+  int angle; //must be integer
+  unsigned long previousMillis;
+  unsigned long previousDelayMillis;
+  int numMovements;
+  int movementIndex;
+  int angles[MAX_MOVEMENTS]; //degree
+  int delays[MAX_MOVEMENTS]; //sec
+  int speeds[MAX_MOVEMENTS]; //degree/sec
+
+  int initialAngle;
+  int resetSpeed;
+};
+
+struct Movement {
+  int ang;
+  int del;
+  int speed;
+};
+
+ServoData servos[NUM_SERVOS];
+
+void initializeServos() {
+
+  servos[0].initialAngle = 0; 
+  servos[1].initialAngle = 0;
+  servos[2].initialAngle = 0;
+  servos[3].initialAngle = 0; 
+  servos[4].initialAngle = 2; //ARM
+  
+  servos[0].resetSpeed = 1000; 
+  servos[1].resetSpeed = 1000;
+  servos[2].resetSpeed = 1000;
+  servos[3].resetSpeed = 1000; 
+  servos[4].resetSpeed = 25; //ARM
+
+  servos[0].servo.attach(3,servos[0].initialAngle); 
+  servos[1].servo.attach(5,servos[1].initialAngle);
+  servos[2].servo.attach(6,servos[2].initialAngle);
+  servos[3].servo.attach(9,servos[3].initialAngle); 
+  servos[4].servo.attach(10,servos[4].initialAngle); //ARM
+
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    servos[i].previousMillis = 0;
+    servos[i].previousDelayMillis = 0;
+    servos[i].numMovements = 0;
+    servos[i].movementIndex = 0;
+    memset(servos[i].angles, 0, sizeof(servos[i].angles));
+    memset(servos[i].delays, 0, sizeof(servos[i].delays));
+    servos[i].previousAngle = servos[i].initialAngle;
+    servos[i].angle = servos[i].initialAngle;
+  }
+}
+
+void addMovementServo(int servoIndex, Movement path[], int numEl) {
+  servos[servoIndex].movementIndex = 0;
+  servos[servoIndex].numMovements = numEl;
+  
+  for (int i = 0; i < servos[servoIndex].numMovements; i++) {
+    servos[servoIndex].angles[i] = path[i].ang;
+    servos[servoIndex].delays[i] = path[i].del;
+    servos[servoIndex].speeds[i] = path[i].speed;
+  }
+  servos[servoIndex].angle = servos[servoIndex].angles[0];
+
+  stopAllServos();
+
+  //Reset Servo before starting new movement
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    bool firstOne = true;
+    if(firstOne){
+      servos[i].servo.startEaseTo(servos[i].initialAngle, servos[i].resetSpeed);
+      firstOne = false;
+    }
+    else{
+      servos[i].servo.setEaseTo(servos[i].initialAngle, servos[i].resetSpeed);
+    }
+    servos[i].previousAngle = servos[i].initialAngle;
+  }
+  
+  //Wait until are servo are resetted
+  while (ServoEasing::areInterruptsActive()) {
+    delay(10);
+  }
+
+ 
+  servos[servoIndex].previousMillis = millis();                  
+  servos[servoIndex].previousDelayMillis = millis();
+}
+
+void updateServos(unsigned long currentMillis) {
+  //Move servos
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    bool firstOne = true;
+    if (servos[i].previousAngle != servos[i].angle) {
+      if(firstOne){
+        servos[i].servo.startEaseTo(servos[i].angle, servos[i].speeds[servos[i].movementIndex]);
+        firstOne = false;
+      }
+      else{
+        servos[i].servo.setEaseTo(servos[i].angle, servos[i].speeds[servos[i].movementIndex]);
+      }
+      servos[i].previousAngle = servos[i].angle;
+    }
+  }
+  //Update Angles
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    if (servos[i].movementIndex < servos[i].numMovements - 1) {
+      if ((currentMillis - servos[i].previousDelayMillis) / 1000 >= servos[i].delays[servos[i].movementIndex + 1]) {
+        if(!servos[i].servo.isMoving())
+        servos[i].movementIndex++;
+        servos[i].previousDelayMillis = currentMillis;
+        servos[i].angle = servos[i].angles[servos[i].movementIndex];
+      }
+    }
+  }
+}
+
+bool isCompleteServo(int servoIndex) {
+  return (servos[servoIndex].movementIndex == (servos[servoIndex].numMovements - 1) || servos[servoIndex].numMovements == 0) && servos[servoIndex].previousAngle == servos[servoIndex].angle;
+}
+
+#endif
