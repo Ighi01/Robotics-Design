de305da224dba5340aca3ce77be5b5a46426cc08
kiinami <kinami@posteo.net>
2024-05-26 15:50:00 +0200

    feat: added sounds

--- main/raspberry_pi/robot/sounds.py
+++ main/raspberry_pi/robot/sounds.py
@@ -1,5 +1,28 @@
 from enum import Enum
 import pygame
 
+
 class Sounds(Enum):
+    AAAA = pygame.mixer.Sound('static/audio/aaaa.wav')
+    ANGRY = pygame.mixer.Sound('static/audio/angry.wav')
+    BLEAH = pygame.mixer.Sound('static/audio/bleah.wav')
+    BLEEP = pygame.mixer.Sound('static/audio/bleep.wav')
+    BURP = pygame.mixer.Sound('static/audio/burp.wav')
+    COUGH = pygame.mixer.Sound('static/audio/cough.wav')
+    CRYING = pygame.mixer.Sound('static/audio/crying.wav')
+    EATING = pygame.mixer.Sound('static/audio/eating.wav')
+    EVIL_LAUGH = pygame.mixer.Sound('static/audio/evil_laugh.wav')
+    EXHALE = pygame.mixer.Sound('static/audio/exhale.wav')
+    FRUSTRATED = pygame.mixer.Sound('static/audio/frustrated.wav')
+    GROAN = pygame.mixer.Sound('static/audio/groan.wav')
+    GROWL = pygame.mixer.Sound('static/audio/growl.wav')
     GRRR = pygame.mixer.Sound('static/audio/grrr.wav')
+    GRUMBLE = pygame.mixer.Sound('static/audio/grumble.wav')
+    HIGH_LAUGH = pygame.mixer.Sound('static/audio/high_laugh.wav')
+    INHALE = pygame.mixer.Sound('static/audio/inhale.wav')
+    RASP = pygame.mixer.Sound('static/audio/rasp.wav')
+    SONG = pygame.mixer.Sound('static/audio/song.wav')
+    SWALLOW = pygame.mixer.Sound('static/audio/swallow.wav')
+    UHOH = pygame.mixer.Sound('static/audio/uhoh.wav')
+    YUM = pygame.mixer.Sound('static/audio/yum.wav')
+    YUMMY = pygame.mixer.Sound('static/audio/yummy.wav')
